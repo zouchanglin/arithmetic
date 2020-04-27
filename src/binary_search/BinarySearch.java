fc91d860e8857d993ef8fdf803c63e32dbd5a108
zouchanglin <zchanglin@aliyun.com>
2020-04-27 11:53:26 +0800

    feat:AtomicReference

--- src/binary_search/BinarySearch.java
+++ src/binary_search/BinarySearch.java
@@ -1,6 +1,17 @@
 package binary_search;
 
+import imooc.sort.SortTestHelper;
+
 public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = SortTestHelper.generate(10, 0, 10);
+        int ret = binarySearchTwo(arr, 3);
+        if(ret != -1){
+            System.out.println("arr[" + ret + "] = " + arr[ret]);
+        }else{
+            System.out.println("Not Exist");
+        }
+    }
     public boolean Find(int target, int [][] array) {
         if(array.length == 0) return false;
         for (int[] ints : array) {
@@ -19,13 +30,28 @@ public class BinarySearch {
         int left = 0;
         int right = arr.length - 1;
         while(left <= right){
-            int zh = (right - left)/2 + left;
-            if(target > arr[zh]){
-                left = zh+1;
-            }else if(target < arr[zh]){
-                right = zh-1;
+            int mid = (right - left)/2 + left;
+            if(target > arr[mid]){
+                left = mid + 1;
+            }else if(target < arr[mid]){
+                right = mid - 1;
             }else{
-                return zh;
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    private static int binarySearchTwo(int[] arr, int target){
+        int left = 0;
+        int right = arr.length - 1;
+        while(left <= right){
+            int mid = (right - left)/2 + left;
+            if(arr[mid] == target) return mid;
+            if(arr[mid] > target){
+                right = mid - 1;
+            }else{
+                left = mid + 1;
             }
         }
         return -1;
