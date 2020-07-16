60e828dc8ebcc29e3fa678a8a6f3a751ce274457
zouchanglin <zchanglin3@gmail.com>
2020-07-17 00:44:44 +0800

    feat: 字节跳动面试题，动态规划

---
+++ src/bytedance/first/MaximumBenefits.java
@@ -0,0 +1,22 @@
+package com.bytedance.first;
+
+public class MaximumBenefits {
+    public static void main(String[] args) {
+        int arr[] = {7,1,5,3,6,4};
+        int ret = calc(arr);
+        System.out.println(ret);
+    }
+
+    private static int calc(int[] arr) {
+        if(arr.length < 2) return 0;
+        int maxBenefits = Integer.MIN_VALUE;
+        int minPrice = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            minPrice = Math.min(minPrice, arr[i]);
+            int oldMaxBenefits = maxBenefits;
+            maxBenefits = Math.abs(minPrice - arr[i]);
+            maxBenefits = Math.max(maxBenefits, oldMaxBenefits);
+        }
+        return maxBenefits;
+    }
+}
